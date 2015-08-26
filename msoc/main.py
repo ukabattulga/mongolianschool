5c6fe16e6138ddd1630237dfd8318686429708a7
Uyanga Battulga <yangaa.battulga@gmail.com>
2015-08-26 11:02:56 -0600

    update
    Here is all of the files

---
+++ msoc/main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import jinja2
+import os
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        entry = jinja_environment.get_template('/templates/mongolianschool.html')
+        self.response.write(entry.render())
+
+
+
+# class ResponseHandler(webapp2.RequestHandler):
+#     def post(self):
+#         team_members = {
+#             'Max': {'college': 'UCM'},
+#             'Bonnie': {'college': 'UofW'},
+#             'Vikki': {'college': 'UCI'},
+#             'Uka': {'college':'Brown'},
+#             'Cydney': {'college':'USC'}
+#             }
+#         adjective1 = self.request.get('adjective1')
+#         verb1 = self.request.get('verb1')
+#         noun1 = self.request.get('pluralnoun1')
+#         noun2 = self.request.get('noun1')
+#         dict_words = {
+#             'adjective1': adjective1,
+#             'verb1': verb1,
+#             'pluralnoun1': noun1,
+#             'noun1': noun2,
+#             'team_members': team_members
+#             }
+#         page = jinja_environment.get_template('templates/response.html')
+#         self.response.write(page.render(dict_words))
+
+
+
+
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname (__file__)))
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    # ('/response', ResponseHandler)
+], debug=True)
+
---
+++ msoc/stylesheets/mongolianschool.js
@@ -0,0 +1,28 @@
+// fixed menu scroll
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+// slideshow
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      3000);
+});
